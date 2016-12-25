15905e569b8bc285143516734f669233d2760c01
disoul <starkyds@gmail.com>
2016-12-25 15:27:08 +0800

    demo1 finished

--- src/main.js
+++ src/main.js
@@ -5,10 +5,18 @@
  * Distributed under terms of the MIT license.
  */
 import './postcss/index.css';
+import vecData from './getVec';
+import Flow from './flow';
 
-import './draw';
 
+console.log(vecData);
 document.write(
   '<script src="http://' + (location.host || 'localhost').split(':')[0] +
   ':35729/livereload.js?snipver=1"></script>'
 );
+
+let flow = new Flow(document.getElementById('container'), 10000);
+
+document.getElementById('enter').addEventListener('click', e => {
+  flow.render(vecData);
+});
